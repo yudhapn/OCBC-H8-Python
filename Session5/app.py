16378836459d6083b421baaa1681487a3a65990f
stormborax <stormborax@gmail.com>
2021-11-10 09:43:04 +0700

    added codes from session5

---
+++ Session5/app.py
@@ -0,0 +1,19 @@
+from dog import *
+
+miles = Dog("Miles", 4)
+buddy = Dog("Buddy", 9)
+jack = Dog("Jack", 3)
+jim = Dog("Jim", 5)
+
+print(miles)
+print(miles.speak("Woof Woof"))
+print(buddy.speak("Yap"))
+
+miles = JackRussellTerrier("Miles", 4)
+buddy = Dachshund("Buddy", 9)
+jack = Bulldog("Jack", 3)
+jim = Bulldog("Jim", 5)
+
+print(jack)
+print(miles.speak())
+print(miles.speak("Grrr"))
